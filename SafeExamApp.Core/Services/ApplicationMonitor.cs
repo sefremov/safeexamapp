af7c6a9da56516b1e7bbb86a41e8f33a4b9d3603
Kirill Mayboroda <mayboroda-ka@m13.su>
2020-06-16 21:33:39 +0300

    Get window title on macOS

--- SafeExamApp.Core/Services/ApplicationMonitor.cs
+++ SafeExamApp.Core/Services/ApplicationMonitor.cs
@@ -1,47 +1,80 @@
 ﻿using SafeExamApp.Core.Interfaces;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 
-namespace SafeExamApp.Core.Services {
-    class ApplicationMonitor : IApplicationMonitor {
-
+namespace SafeExamApp.Core.Services
+{
+    class ApplicationMonitor : IApplicationMonitor
+    {
         [DllImport("user32.dll")]
         static extern IntPtr GetForegroundWindow();
 
         [DllImport("user32.dll")]
         static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
 
-        private string GetActiveWindowTitleWin() {
+        private string GetActiveWindowTitleWin()
+        {
             const int nChars = 256;
             StringBuilder Buff = new StringBuilder(nChars);
             IntPtr handle = GetForegroundWindow();
 
-            if(GetWindowText(handle, Buff, nChars) > 0) {
+            if (GetWindowText(handle, Buff, nChars) > 0) {
                 return Buff.ToString();
             }
+
             return null;
         }
 
-        private string GetActiveWindowTitleOSX() {
-            return null;
+        private string GetActiveWindowTitleMacOS()
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/sh",
+                    Arguments = $"-c \"{MacOsExecutablePath} {MacOsScriptPath}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+
+            var output = process.StandardOutput;
+
+            process.WaitForExit();
+
+            return output.ReadToEnd();
         }
+
+        private const string MacOsExecutablePath = "/usr/bin/osascript";
+
+        private static readonly string MacOsScriptPath = Path.Combine(
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,
+            @"Resources/GetActiveWindow.scrt"
+        );
 
         private string currentActive;
 
         public event Action<string> OnActiveWindowChanged;
 
-        public void CheckActiveApplication() {
+        public void CheckActiveApplication()
+        {
             string active;
-            if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                active = GetActiveWindowTitleOSX();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                active = GetActiveWindowTitleMacOS();
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 active = GetActiveWindowTitleWin();
             else
                 active = "";
 
-            if (!string.IsNullOrWhiteSpace(active) && active != currentActive) {
+            if (!string.IsNullOrWhiteSpace(active) && active != currentActive)
+            {
                 OnActiveWindowChanged?.Invoke(active);
                 currentActive = active;
             }
